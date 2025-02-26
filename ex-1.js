e8acdf7552f2975815367ee5f02f590603ddfcb4
Binwaran <binwaran.m@gmail.com>
2025-02-26 18:56:39 +0800

    Add: Exercise 1-2

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,19 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20); //false
-Object.is(0, -0); //false
-Object.is(NaN, 100); //false
-Object.is("one", "two"); //false
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false
-Object.is(null, undefined); //false
+Object.is(10, 20); //false //false
+Object.is(0, -0); //false //false
+Object.is(NaN, 100); //false //false
+Object.is("one", "two"); //false //false
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false //false
+Object.is(null, undefined); //false //false
 
-10 === 20; // false
-10 === "10" //false
-3.14 !== 3.14;  //false
-"TechUp" !== "TechUp"; //false
-NaN === NaN; //false
-0 !== -0; //false
+10 === 20; //false // false
+10 === "10" //false //false
+3.14 !== 3.14;  //false //false
+"TechUp" !== "TechUp"; //false //false
+NaN === NaN; //false //false
+0 !== -0; //false //false
 
 let objectA = { course: "Web Developer", price: 300000 }; 
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB; //false
+objectA === objectB; //false //false
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false; //true
-null == undefined; //true
-10 == "10"; //true
-[10] == 10; //true
-[1,2] == "1,2"; //true
+0 == false; // true //true
+null == undefined; // true //true
+10 == "10"; // true //true
+[10] == 10; // true //true
+[1,2] == "1,2"; // true //true
